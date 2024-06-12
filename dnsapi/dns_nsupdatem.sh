96bbb60179be518a756ef0d7c933ac6bcb65d130
David唐辉 <david921518@qq.com>
2024-06-12 10:36:11 +0800

    dnsapi: add dns_nsupdatem.sh
    dns_nsupdatem.sh support multiple name servers to update.

---
+++ dnsapi/dns_nsupdatem.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env sh
+
+########  Public functions #####################
+
+#Usage: dns_nsupdatem_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+dns_nsupdatem_add() {
+  fulldomain=$1
+  txtvalue=$2
+  NSUPDATE_SERVER="${NSUPDATE_SERVER:-$(_readaccountconf_mutable NSUPDATE_SERVER)}"
+  NSUPDATE_SERVER_PORT="${NSUPDATE_SERVER_PORT:-$(_readaccountconf_mutable NSUPDATE_SERVER_PORT)}"
+  NSUPDATE_SERVER2="${NSUPDATE_SERVER2:-$(_readaccountconf_mutable NSUPDATE_SERVER2)}"
+  NSUPDATE_SERVER2_PORT="${NSUPDATE_SERVER2_PORT:-$(_readaccountconf_mutable NSUPDATE_SERVER2_PORT)}"
+  NSUPDATE_KEY="${NSUPDATE_KEY:-$(_readaccountconf_mutable NSUPDATE_KEY)}"
+  NSUPDATE_ZONE="${NSUPDATE_ZONE:-$(_readaccountconf_mutable NSUPDATE_ZONE)}"
+
+  _checkKeyFile || return 1
+
+  # save the dns server and key to the account conf file.
+  _saveaccountconf_mutable NSUPDATE_SERVER "${NSUPDATE_SERVER}"
+  _saveaccountconf_mutable NSUPDATE_SERVER_PORT "${NSUPDATE_SERVER_PORT}"
+  _saveaccountconf_mutable NSUPDATE_SERVER2 "${NSUPDATE_SERVER2}"
+  _saveaccountconf_mutable NSUPDATE_SERVER2_PORT "${NSUPDATE_SERVER2_PORT}"
+  _saveaccountconf_mutable NSUPDATE_KEY "${NSUPDATE_KEY}"
+  _saveaccountconf_mutable NSUPDATE_ZONE "${NSUPDATE_ZONE}"
+
+  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
+  [ -n "${NSUPDATE_SERVER_PORT}" ] || NSUPDATE_SERVER_PORT=53
+
+  _info "adding ${fulldomain}. 60 in txt \"${txtvalue}\""
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_1" ] && nsdebug="-d"
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_2" ] && nsdebug="-D"
+  if [ -z "${NSUPDATE_ZONE}" ]; then
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+update add ${fulldomain}. 60 in txt "${txtvalue}"
+send
+EOF
+  else
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+zone ${NSUPDATE_ZONE}.
+update add ${fulldomain}. 60 in txt "${txtvalue}"
+send
+EOF
+  fi
+  if [ $? -ne 0 ]; then
+    _err "error updating domain"
+    return 1
+  fi
+
+
+ if [ -z "${NSUPDATE_SERVER2}" ]; then
+  echo "no NSUPDATE_SERVER2"
+ else
+  echo "has NSUPDATE_SERVER2"
+  [ -n "${NSUPDATE_SERVER2_PORT}" ] || NSUPDATE_SERVER2_PORT=53
+  _info "adding ${fulldomain}. 60 in txt \"${txtvalue}\" to ${NSUPDATE_SERVER2}"
+
+  if [ -z "${NSUPDATE_ZONE}" ]; then
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER2}  ${NSUPDATE_SERVER_PORT2}
+update add ${fulldomain}. 60 in txt "${txtvalue}"
+send
+EOF
+  else
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER2}  ${NSUPDATE_SERVER_PORT2}
+zone ${NSUPDATE_ZONE}.
+update add ${fulldomain}. 60 in txt "${txtvalue}"
+send
+EOF
+  fi
+  if [ $? -ne 0 ]; then
+    _err "error updating domain"
+    return 1
+  fi
+ fi
+
+  return 0
+}
+
+#Usage: dns_nsupdatem_rm   _acme-challenge.www.domain.com
+dns_nsupdatem_rm() {
+  fulldomain=$1
+
+  NSUPDATE_SERVER="${NSUPDATE_SERVER:-$(_readaccountconf_mutable NSUPDATE_SERVER)}"
+  NSUPDATE_SERVER_PORT="${NSUPDATE_SERVER_PORT:-$(_readaccountconf_mutable NSUPDATE_SERVER_PORT)}"
+  NSUPDATE_SERVER2="${NSUPDATE_SERVER2:-$(_readaccountconf_mutable NSUPDATE_SERVER2)}"
+  NSUPDATE_SERVER2_PORT="${NSUPDATE_SERVER2_PORT:-$(_readaccountconf_mutable NSUPDATE_SERVER2_PORT)}"
+  NSUPDATE_KEY="${NSUPDATE_KEY:-$(_readaccountconf_mutable NSUPDATE_KEY)}"
+  NSUPDATE_ZONE="${NSUPDATE_ZONE:-$(_readaccountconf_mutable NSUPDATE_ZONE)}"
+
+  _checkKeyFile || return 1
+  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
+  [ -n "${NSUPDATE_SERVER_PORT}" ] || NSUPDATE_SERVER_PORT=53
+  _info "removing ${fulldomain}. txt"
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_1" ] && nsdebug="-d"
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_2" ] && nsdebug="-D"
+  if [ -z "${NSUPDATE_ZONE}" ]; then
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+update delete ${fulldomain}. txt
+send
+EOF
+  else
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+zone ${NSUPDATE_ZONE}.
+update delete ${fulldomain}. txt
+send
+EOF
+  fi
+  if [ $? -ne 0 ]; then
+    _err "error updating domain"
+    return 1
+  fi
+
+ if [ -z "${NSUPDATE_SERVER2}" ]; then
+  echo "no NSUPDATE_SERVER2"
+ else
+  echo "has NSUPDATE_SERVER2"
+  [ -n "${NSUPDATE_SERVER2_PORT}" ] || NSUPDATE_SERVER2_PORT=53
+  _info "removing ${fulldomain}. txt from ${NSUPDATE_SERVER2}"
+
+  if [ -z "${NSUPDATE_ZONE}" ]; then
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER2}  ${NSUPDATE_SERVER2_PORT}
+update delete ${fulldomain}. txt
+send
+EOF
+  else
+    nsupdate -k "${NSUPDATE_KEY}" $nsdebug <<EOF
+server ${NSUPDATE_SERVER2}  ${NSUPDATE_SERVER2_PORT}
+zone ${NSUPDATE_ZONE}.
+update delete ${fulldomain}. txt
+send
+EOF
+  fi
+  if [ $? -ne 0 ]; then
+    _err "error updating domain"
+    return 1
+  fi
+ fi
+
+  return 0
+}
+
+####################  Private functions below ##################################
+
+_checkKeyFile() {
+  if [ -z "${NSUPDATE_KEY}" ]; then
+    _err "you must specify a path to the nsupdate key file"
+    return 1
+  fi
+  if [ ! -r "${NSUPDATE_KEY}" ]; then
+    _err "key ${NSUPDATE_KEY} is unreadable"
+    return 1
+  fi
+}
